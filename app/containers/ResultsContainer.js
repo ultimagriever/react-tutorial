6b4d0dbf2c279aff15041828ad42b9a93b0e38fe
Pamela Argentino <argentinopam@hotmail.com>
2016-07-01 15:47:20 -0300

    Add basic results calculation

---
+++ app/containers/ResultsContainer.js
@@ -0,0 +1,31 @@
+var React = require('react');
+var Results = require('../components/Results');
+var GithubHelpers = require('../utils/GithubHelpers');
+
+var ResultsContainer = React.createClass({
+  getInitialState: function() {
+    return {
+      isLoading: true,
+      scores: []
+    }
+  },
+  componentDidMount: function() {
+    GithubHelpers.battle(this.props.location.state.playersInfo)
+      .then(function(scores) {
+        this.setState({
+          scores: scores,
+          isLoading: false
+        })
+      }.bind(this));
+  },
+  render: function() {
+    return (
+      <Results
+        isLoading={this.state.isLoading}
+        scores={this.state.scores}
+        playersInfo={this.props.location.state.playersInfo} />
+    );
+  }
+});
+
+module.exports = ResultsContainer;
--- app/utils/GithubHelpers.js
+++ app/utils/GithubHelpers.js
@@ -5,21 +5,59 @@ var sec = "ed278f1ae47279243f4724a6794dd42b857122f4";
 
 var param = "?client_id=" + id + "&client_secret=" + sec;
 
-function getUserInfo(username) {
+function getUserInfo (username) {
   return axios.get('https://api.github.com/users/' + username + param);
 }
 
+function getRepos (username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
+}
+
+function getTotalStars (repos) {
+  return repos.data.reduce(function (previous, current) {
+    return previous + current.stargazers_count;
+  }, 0);
+}
+
+function getPlayersData (player) {
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function (totalStars) {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    });
+}
+
+function calculateScores (players) {
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ];
+}
+
 var helpers = {
-  getPlayersInfo: function(players) {
-    return axios.all(players.map(function(username) {
+  getPlayersInfo: function (players) {
+    return axios.all(players.map(function (username) {
       return getUserInfo(username);
-    })).then(function(info) {
-      return info.map(function(user) {
+    })).then(function (info) {
+      return info.map(function (user) {
         return user.data;
       });
-    }).catch(function(err) {
+    }).catch(function (err) {
       console.warn('Error in getPlayersInfo', err);
     });
+  },
+  battle: function (players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function(err) {
+        console.warn('Error in getPlayersData: ' + err);
+      })
   }
 };
 
